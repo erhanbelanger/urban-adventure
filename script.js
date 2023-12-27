5b61c844bf4225b1e819796553ad5b95181bef99
Erhan <erhan.belanger25@gmail.com>
2023-12-26 21:00:49 -0500

    fixing javascript

--- script.js
+++ script.js
@@ -1,148 +1,67 @@
-//adding variables and attaching them to their classes from HTML
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonElement = document.getElementById('answer-buttons')
+let timeLeft =document.querySelector('.time-left');
+let quizContainer = document.getElementById('container');
+let nextBtn = document.getElementById('next-button');
+let numberOfQuestions = document.querySelector('.questions');
+let displayContainer = document.getElementById('display-container');
+let scoreContainer = document.querySelector('.score-container');
+let restart = document.getElementById('restart');
+let userScore = document.getElementById('user-score');
+let startScreen = document.getElementById('user-score');
+let startButton = document.getElementById('start-button');
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
 
-let shuffledQuestions, currentQuestionIndex, timer
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-    clearTimeout(timer)
-currentQuestionIndex++
-setNextQuestion()
-})
-//starting game
-function startGame() {
-console.log('Started')
-startButton.classList.add('hide')
-shuffledQuestions = questions.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
-}
-// setting next question and timer consequences
-function setNextQuestion() {
-    resetState()
-    if (currentQuestionIndex < shuffledQuestions.length) {
-        showQuestion(shuffledQuestions[currentQuestionIndex]);
-        startTimer();
-    } else {
-        endgame();
-    }
-showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-//setting timer button
-function startTimer() {
-    let seconds = 60;
-    timer = setInterval(() => {
-seconds -= 5;
-if (seconds < 0) {
-    clearTimeout(timer);
-    selectAnswer();
-}
-    }, 5000);
-}
-
-function showQuestion(question) {
-questionElement.innerText = question.question
-question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-        button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonElement.appendChild(button)
-});
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonElement.firstChild) {
-        answerButtonElement.removeChild
-        (answerButtonElement.firstChild)
-    }
-}
-
-function selectAnswer(e) {
-    clearTimeout(timer);
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide')
-    } else {
-        startButton.innerText = 'restart'
-        startButton.classList.remove('hide')
-    }
-    nextButton.classList.remove('hide')
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
-
-const questions = [
+const quizArray = [
     {
         question: 'Commonly used data types DO NOT include:',
-        answers: [
-            {text: 'alerts', correct: true},
-            {text: 'booleans', correct: false},
-            {text: 'alerts', correct: false },
-            {text: 'numbers', correct: false}
-        ] 
+        options: [
+             'alerts',
+             'booleans', 
+             'alerts', 
+             'numbers', 
+        ],
+        correct : 'alerts', 
     },
     {
         question: 'Inside which HTML element do we put the JavaSCript?:',
-        answers: [
-            {text: '<script>', correct: true},
-            {text: '<javascript', correct: false},
-            {text: '<js>', correct: false },
-            {text: '<scripting>', correct: false}
-        ] 
+        options: [
+             '<script>',
+             '<javascript', 
+             '<js>', 
+             '<scripting>', 
+        ],
+        correct : '<script>',
     },
     {
         question: 'JavaScript is a ___-side programming language:',
-        answers: [
-            {text: 'none', correct: false},
-            {text: 'server', correct: false},
-            {text: 'both', correct: true },
-            {text: 'client', correct: false}
-        ] 
+        options: [
+            'none', 
+            'server', 
+            'both', 
+            'client', 
+        ],
+        correct : 'both', 
     },
     {
         question: 'Which of the following will write the message "hello world!" in an alert box?:',
-        answers: [
-            {text: 'alertBox("hello world!");', correct: false},
-            {text: 'alert("hello world!");', correct: true},
-            {text: 'alert(hello world!);', correct: false },
-            {text: 'msgAlert("hello world!");', correct: false}
-        ] 
+        options: [
+            'alertBox("hello world!");', 
+            'alert("hello world!");', 
+            'alert(hello world!);', 
+            'msgAlert("hello world!");', 
+        ],
+        correct: 'alert(hello world!);'
     },
     {
         question: 'Which of the following JavaScript labels catches all the values, except for the ones specified?',
-        answers: [
-            {text: 'default', correct: true},
-            {text: 'catch', correct: false},
-            {text: 'label', correct: false },
-            {text: 'try', correct: false}
-        ] 
+        options: [
+             'default',
+             'catch',
+             'label',
+             'try',
+        ] ,
+        correct: 'default',
     },
 ]
